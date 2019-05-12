98b798b4b0f7a5b8e2c64035152cef491bf9bde8
北爪勇気 <kitazume@uk-mini.local>
2019-05-12 21:45:54 +0900

    更新

--- .sfdx/typings/lwc/apex/ImportantNotesController.d.ts
+++ .sfdx/typings/lwc/apex/ImportantNotesController.d.ts
@@ -5,7 +5,7 @@ declare module "@salesforce/apex/ImportantNotesController.findImportantNotes" {
   export default function findImportantNotes(param: {searchKey: any}): Promise<any>;
 }
 declare module "@salesforce/apex/ImportantNotesController.findImportantNotes2" {
-  export default function findImportantNotes2(param: {searchClass: any, searchYear: any}): Promise<any>;
+  export default function findImportantNotes2(param: {searchValue: any, searchYear: any, searchGroup: any}): Promise<any>;
 }
 declare module "@salesforce/apex/ImportantNotesController.findKindergartenDiary" {
   export default function findKindergartenDiary(param: {searchDate: any}): Promise<any>;
--- force-app/main/default/lwc/staffAttendanceMenu03/staffAttendanceMenu03.js
+++ force-app/main/default/lwc/staffAttendanceMenu03/staffAttendanceMenu03.js
@@ -1,30 +1,42 @@
-/* eslint-disable no-console */
 import { LightningElement,wire,track } from 'lwc';
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import TYPE_Class from 'staffAttendanceMenu03/node_modules/@salesforce/schema/ImportantNotes__c.Class__c';
-import findImportantNotes from 'staffAttendanceMenu03/node_modules/@salesforce/apex/ImportantNotesController.findImportantNotes2';
-import findKindergartenDiary from 'staffAttendanceMenu03/node_modules/@salesforce/apex/ImportantNotesController.findKindergartenDiary';
-/* ポップアップメッセージ表示 */
-import {ShowToastEvent} from 'staffAttendanceMenu03/node_modules/lightning/platformShowToastEvent';
-/* /ポップアップメッセージ表示 */
 
-/* 要録アップデート */
+/* 選択リストを取得 */
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import TYPE_Class from '@salesforce/schema/ImportantNotes__c.Class__c';
+import BUSTYPE_Class from '@salesforce/schema/Contact.PassageRoute__c';
+
+import findImportantNotes from '@salesforce/apex/ImportantNotesController.findImportantNotes2';
+import findKindergartenDiary from '@salesforce/apex/ImportantNotesController.findKindergartenDiary';
+
+/* ポップアップメッセージ表示 */
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+
+/* 日報アップデート */
 import { createRecord } from 'lightning/uiRecordApi';
 import { updateRecord } from 'lightning/uiRecordApi';
-import KindergartenDiary_OBJECT from 'staffAttendanceMenu03/node_modules/@salesforce/schema/KindergartenDiary__c';
-import ID_FIELD from 'staffAttendanceMenu03/node_modules/@salesforce/schema/KindergartenDiary__c.Id';
-import AS_FIELD from 'staffAttendanceMenu03/node_modules/@salesforce/schema/KindergartenDiary__c.AttendanceSchedule__c';
-import ENGJIID_FIELD from 'staffAttendanceMenu03/node_modules/@salesforce/schema/KindergartenDiary__c.Contact__c';
-import DATE_FIELD from 'staffAttendanceMenu03/node_modules/@salesforce/schema/KindergartenDiary__c.Date__c';
-/* 要録アップデート */
+import KindergartenDiary_OBJECT from '@salesforce/schema/KindergartenDiary__c';              //園児日誌obj
+import ID_FIELD from '@salesforce/schema/KindergartenDiary__c.Id';                           //園児日誌obj:園児日誌ID
+import AS_FIELD from '@salesforce/schema/KindergartenDiary__c.AttendanceSchedule__c';        //園児日誌obj:出欠席予定
+import ENGJIID_FIELD from '@salesforce/schema/KindergartenDiary__c.Contact__c';              //園児日誌obj:園児名
+import DATE_FIELD from '@salesforce/schema/KindergartenDiary__c.Date__c';                    //園児日誌obj:日付
+import ASTIME_FIELD from '@salesforce/schema/KindergartenDiary__c.AttendanceTime__c';        //園児日誌obj:出席時間
+import TOUEN_FIELD from '@salesforce/schema/KindergartenDiary__c.AttendingSchool__c';        //園児日誌obj:登園
+import TOUENTIME_FIELD from '@salesforce/schema/KindergartenDiary__c.attendingSchoolTime__c';//園児日誌obj:登園時間
+import KOUEN_FIELD from '@salesforce/schema/KindergartenDiary__c.GoingBack__c';              //園児日誌obj:降園
+import KOUENTIME_FIELD from '@salesforce/schema/KindergartenDiary__c.GoingBackTime__c';      //園児日誌obj:降園時間
+
+
+
 
 
 export default class StaffAttendanceMenu03 extends LightningElement {
-    today = new Date() ;
-    todaya = new Date() ;
+    today = new Date();
+    @track ClassList;
+    @track BusList;
+    @track firstDisplay = true;
     @track searchDate = this.today.getFullYear() + "-" + (this.today.getMonth() + 1) + "-"+ this.today.getDate();
     @track titleDate = this.today.getFullYear() + "年" + (this.today.getMonth() + 1) + "月"+ this.today.getDate() + "日の出席簿";
-    @track searchYear = this.todaya.getFullYear();
+    @track searchYear = this.today.getFullYear();
     @track importantNotes;
     @track error = false;
 
@@ -38,6 +50,39 @@ export default class StaffAttendanceMenu03 extends LightningElement {
     picklistValues;
 
     /* ------------------------
+        バス通園コースを取得：初回のみ
+    ------------------------ */
+    @wire(getPicklistValues, {
+        recordTypeId: '012000000000000AAA',
+        fieldApiName: BUSTYPE_Class
+    })
+    PickBuslist;
+
+    //現在時刻取得（yyyy/mm/dd hh:mm:ss）
+    getCurrentTime() {
+        var now = new Date();
+        var res;
+        now.setHours(now.getHours() - 9);//Salesforceに取り込んだときにずれる
+        res = "" + now.getFullYear() + "-" + this.padZero(now.getMonth() + 1) + 
+            "-" + this.padZero(now.getDate()) + "T" + this.padZero(now.getHours()) + ":" + 
+            this.padZero(now.getMinutes()) + ":" + this.padZero(now.getSeconds()) + ".000Z";
+        return res;
+    }
+
+    //先頭ゼロ付加
+    padZero(num) {
+        var result;
+        if (num < 10) {
+            result = "0" + num;
+        } else {
+            result = "" + num;
+        }
+        return result;
+    }
+
+
+
+    /* ------------------------
         園児リストを取得
     ------------------------ */
     links(event){ 
@@ -45,16 +90,17 @@ export default class StaffAttendanceMenu03 extends LightningElement {
         var KindergartenDiaryList;  //園児日報一覧を一旦格納
         var i;                      //forループ用1
         var i2;                     //forループ用2
-        var setsCheck;              //forループで要録と日報がマッチしたかどうかをチェック
-
+        //var setsCheck;              //forループで要録と日報がマッチしたかどうかをチェック
+        var group =event.target.dataset.group;//クラス選択かバス選択か
         //並列処理?的な
         async function merge(searchYear,searchDate){
             //クラスで条件を絞り要録一覧を取得
             ImportantNoteList = await (
                 //Apexを呼び出し
                 findImportantNotes({
-                        searchClass: event.target.dataset.value,
-                        searchYear: searchYear
+                        searchValue: event.target.dataset.value,
+                        searchYear: searchYear,
+                        searchGroup: event.target.dataset.group
                     })
                     .then(result => {
                         return result;
@@ -87,19 +133,35 @@ export default class StaffAttendanceMenu03 extends LightningElement {
                 //お互いの園児ID（Contact.ID）がマッチしたらImportantNoteList側に日報情報（日報IDと現在の出席情報）を追記する
                 for(i = 0; i< ImportantNoteList.length; i++){
                     
-                    setsCheck =false;   //マッチ情報を初期化
+                    //setsCheck =false;   //マッチ情報を初期化
+
                     for(i2 = 0; i2< KindergartenDiaryList.length; i2++){
                         if(ImportantNoteList[i].Contact__r.Id === KindergartenDiaryList[i2].Contact__r.Id){
                             ImportantNoteList[i].kindergartenDiaryId = KindergartenDiaryList[i2].Id;
                             ImportantNoteList[i].AttendanceSchedule = KindergartenDiaryList[i2].AttendanceSchedule__c;
-                            setsCheck =true;//マッチしたらtrue
+                            if(KindergartenDiaryList[i2].AttendingSchool__c){ImportantNoteList[i].AttendingSchool='登園'}
+                            if(KindergartenDiaryList[i2].GoingBack__c){ImportantNoteList[i].GoingBack='降園'}
+                            //setsCheck =true;//マッチしたらtrue
                         }
                     }
-                    //もし要録に日報がマッチしなかったら出席情報に「未定」といれる
-                    if(!(setsCheck)){ImportantNoteList[i].AttendanceSchedule='未定'}
+                    
+                    if(!(ImportantNoteList[i].AttendanceSchedule)){ImportantNoteList[i].AttendanceSchedule='未定'}
+                    if(!(ImportantNoteList[i].AttendingSchool)){ImportantNoteList[i].AttendingSchool='ーー'}
+                    if(!(ImportantNoteList[i].GoingBack)){ImportantNoteList[i].GoingBack='ーー'}
+                    
                     
                 }
+
                 this.importantNotes = ImportantNoteList;    //表示用の配列にImportantNoteListを入れる
+                this.firstDisplay = false;
+                //画面表示・非表示の切り替え
+                if(group === 'bus'){
+                    this.BusList = true;
+                    this.ClassList = false;
+                }else{
+                    this.BusList = false;
+                    this.ClassList = true;
+                }            
             }
         )
     }
@@ -111,6 +173,8 @@ export default class StaffAttendanceMenu03 extends LightningElement {
         var indexs;             //何番目をクリックしたか把握
         var i ;                 //ループ用
         var recordInput = {};   //レコード作成・更新用
+        var timesstump = this.getCurrentTime();
+        var AttendingSchool = 'ーー';var GoingBack = 'ーー';
         event.preventDefault();
 
         //  this.importantNotes.data.Id -> クリックした要素のカスタム要素 data-Idのこと
@@ -123,24 +187,47 @@ export default class StaffAttendanceMenu03 extends LightningElement {
         }
 
         /* 日報レコード作成や更新に必要な項目を代入  */
+        /* バス（登園）かバス（降園）かクラスかでセットする項目を変える */
         const fields = {};
-            fields[ID_FIELD.fieldApiName] = event.target.dataset.kindergartendiaryid;
-            fields[AS_FIELD.fieldApiName] = event.target.dataset.value;
-            fields[ENGJIID_FIELD.fieldApiName] = this.importantNotes[indexs].Contact__r.Id;
-            fields[DATE_FIELD.fieldApiName] = this.searchDate;                              //今日の日付を入れておく
+        /* 共通 */
+            fields[ID_FIELD.fieldApiName] = event.target.dataset.kindergartendiaryid;       //日報ID
+            fields[DATE_FIELD.fieldApiName] = this.searchDate;                              //今日の日付
+            fields[ENGJIID_FIELD.fieldApiName] = this.importantNotes[indexs].Contact__r.Id; //関連園児ID
+        /* 出席簿の場合 */
+        if(event.target.dataset.group === 'class'){
+            fields[AS_FIELD.fieldApiName] = event.target.dataset.value;                     //出席予定
+            fields[ASTIME_FIELD.fieldApiName] = timesstump;                                 //出席欠席した時間
+        }
+        /* バス：登園の場合 */
+        if(event.target.dataset.group === 'bus' && event.target.dataset.value === '登園'){
+            fields[TOUEN_FIELD.fieldApiName] = true;                                        //登園チェック
+            fields[TOUENTIME_FIELD.fieldApiName] = timesstump;                              //登園時間
+            AttendingSchool= '登園';
+        }
+        /* バス：降園の場合 */
+        if(event.target.dataset.group === 'bus' && event.target.dataset.value === '降園'){
+            fields[KOUEN_FIELD.fieldApiName] = true;                                        //降園チェック
+            fields[KOUENTIME_FIELD.fieldApiName] = timesstump;                              //降園時間 
+            GoingBack= '降園';
+        }
+        if(this.importantNotes[indexs].AttendingSchool === '登園'){AttendingSchool= '登園'}
+        if(this.importantNotes[indexs].GoingBack === '降園'){GoingBack= '降園'}
+
         
         //console.log(fields[ID_FIELD.fieldApiName]);
-        
         //もし選んだ要録に日報IDがセットされてなかったら日報を作成し、セットされてたら日報を更新
         if(fields[ID_FIELD.fieldApiName]=== undefined){
 
-            recordInput = {apiName: KindergartenDiary_OBJECT.objectApiName, fields };   //作成する情報をセット 
-            createRecord(recordInput)
-            
+            recordInput = {apiName: KindergartenDiary_OBJECT.objectApiName, fields };   //作成する情報をセット
+
+            createRecord(recordInput)  
             .then(result => {   //レコード作成に成功したら
                 //表示用の配列情報を更新する
                 this.importantNotes[indexs].kindergartenDiaryId = result.id;                   //作成したレコードIDを表示用の変数に戻してあげる：再描写
                 this.importantNotes[indexs].AttendanceSchedule = fields[AS_FIELD.fieldApiName];//作成した出席情報を表示用の変数に戻してあげる：再描写
+                this.importantNotes[indexs].AttendingSchool = AttendingSchool;
+                this.importantNotes[indexs].GoingBack = GoingBack;
+                // eslint-disable-next-line no-console
                 console.log('作成OK');
 
             })
@@ -160,6 +247,9 @@ export default class StaffAttendanceMenu03 extends LightningElement {
             updateRecord(recordInput)
             .then(() => {
                 this.importantNotes[indexs].AttendanceSchedule = fields[AS_FIELD.fieldApiName];
+                this.importantNotes[indexs].AttendingSchool = AttendingSchool;
+                this.importantNotes[indexs].GoingBack = GoingBack;
+                // eslint-disable-next-line no-console
                 console.log('更新OK');
             })
             .catch(() => {
@@ -174,6 +264,9 @@ export default class StaffAttendanceMenu03 extends LightningElement {
         
         
       }
+
+
+      
     }
     
 
